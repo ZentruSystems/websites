6e8ec6d78b3b1c2db87633ab663c3d71317f7283
truFelix <trutschnigfelix@gmail.com>
2025-05-28 00:24:17 +0200

    Steam widget & pulsing card shadow on evar
    Signed-off-by: truFelix <trutschnigfelix@gmail.com>

--- next/evar/app/page.tsx
+++ next/evar/app/page.tsx
@@ -2,8 +2,8 @@
 export default function EvarPage() {
 	return <main className="hGrid bPad">
 		<div className="s0 e13 ph-e6 tPad ph-tPadResp hGrid hFill">
-			<div className="s1 e12 ph-s0 ph-e6 ph-hUnitMarg allRoundBig ph-allRoundBigResp bg-l2 Container">
-				<div style={{maxHeight: "75vh"}}>
+			<div id="debris-card" className="s1 e12 ph-s0 ph-e6 ph-hUnitMarg allRoundBig ph-allRoundBigResp bg-l2 Container pulsingShadow" style={{ boxShadow: "#ffffff3d 0px 0px 45px 0px" }}>
+				<div style={{ maxHeight: "75vh" }}>
 					<img className="hFill allRoundBig ph-allRoundBigResp" src="./img/debris_screenshot.png" />
 				</div>
 				<div className="tUnitPad Pad ph-PadResp fg-white-fix bg-d4-fixed">
@@ -15,6 +15,13 @@ export default function EvarPage() {
 						Experience this multi-level story unravel beneath you and get to know one of the big secrets of the colony’s funding.<br />
 						Many people say things have to change.
 					</p>
+				</div>
+				<div className="flex layoutHCenter">
+					<iframe
+						className="Pad"
+						src="https://store.steampowered.com/widget/3732370/?utm_source=evar-homepage&utm_medium=widget"
+						frameBorder="0"
+						style={{ height: "calc(2 * var(--normalPadding) + 190px)", width: "60%" }}></iframe>
 				</div>
 			</div>
 		</div>
