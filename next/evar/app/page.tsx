7ed39e41302b1534eec384f7ca4b90d463b64330
truFelix <trutschnigfelix@gmail.com>
2025-09-25 08:54:42 +0200

    [evar] updating image and text
    Signed-off-by: truFelix <trutschnigfelix@gmail.com>

--- next/evar/app/page.tsx
+++ next/evar/app/page.tsx
@@ -5,15 +5,15 @@ export default function EvarPage() {
 		<div className="s0 e13 ph-e6 tPad ph-tPadResp hGrid hFill">
 			<div id="debris-card" className="s1 e12 ph-s0 ph-e6 ph-hUnitMarg allRoundBig ph-allRoundBigResp bg-l2 Container pulsingShadow" style={{ boxShadow: "#ffffff3d 0px 0px 45px 0px" }}>
 				<div style={{ maxHeight: "75vh" }}>
-					<img className="hFill allRoundBig ph-allRoundBigResp" src="./img/debris_screenshot.png" />
+					<img className="hFill allRoundBig ph-allRoundBigResp" src="./img/debris_screenshot_01.png" />
 				</div>
 				<div className="tUnitPad Pad ph-PadResp fg-white-fix bg-d4-fixed">
 					<h2>Get In — save Mars</h2>
 					<p>
 						After many satellites were sent to orbit, the inevitable mass-crash started, and is spreading like a wildfire.
-						Malcom has to master many critical situations to stay alive to save the station and save the colony.<br />
+						Malcom has to master many critical situations to stay alive to save the station, himself and probably more.<br />
 						<br />
-						Experience this multi-level story unravel beneath you and get to know one of the big secrets of the colony’s funding.<br />
+						Experience this multi-level story unravel beneath you and get to know the secrets of the colony’s founding.<br />
 						Many people say things have to change.
 					</p>
 				</div>
