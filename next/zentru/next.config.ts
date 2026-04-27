6f3642bc6ab3a210892545cca15642e5d55f370c
truFelix <trutschnigfelix@gmail.com>
2026-04-28 00:42:32 +0200

    build
    Signed-off-by: truFelix <trutschnigfelix@gmail.com>

--- next/zentru/next.config.ts
+++ next/zentru/next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  distDir: "out",
+  // distDir: "out",
   transpilePackages: ["common"],
   allowedDevOrigins: [
     "10.0.1.102",
